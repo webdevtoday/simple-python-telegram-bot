3aae1d1a25b11c7318da4cee38961b49e837e8ce
webdevtoday <coderwd@gmail.com>
2022-10-29 02:24:25 +0300

    Fix User-Agent header

--- olx_parser/main.py
+++ olx_parser/main.py
@@ -19,7 +19,7 @@ class OLXParser:
     def __init__(self):
         self.session = requests.Session()
         self.session.headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15',
             'Accept-Language': 'ru',
         }
 
